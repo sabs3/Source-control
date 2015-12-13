678ba9bdca7af05b893212ba26dd7922b7dd6a6c
sabs3 <sabinakhanal@hotmail.co.uk>
2015-12-13 15:28:01 +0000

    maven test

---
+++ src/test/java/ArtistTest.java
@@ -0,0 +1,14 @@
+
+import org.junit.Assert;
+import org.junit.Test;
+/**
+ * Created by sabinakhanal on 13/12/2015.
+ */
+public class ArtistTest {
+
+    @Test
+    public void artistTest() {
+        Assert.assertTrue(true);
+    }
+
+}
